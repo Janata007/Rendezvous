b6ea2be2b5e14e76e8b1a696ebd2b9f0937efe41
Jana Markovikj <jana.markovikj@endava.com>
2022-02-19 13:23:44 +0100

    initial

---
+++ src/main/java/com/finki/ukim/rendezvous/RendezvousApplication.java
@@ -0,0 +1,13 @@
+package com.finki.ukim.rendezvous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendezvousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RendezvousApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finki/ukim/rendezvous/RendezvousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.ukim.rendezvous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendezvousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
