b74d4dab4fb45b41d8a30c6e28a8e0e299e0255b
Marko Markovikj <marko.markovikj@endava.com>
2022-02-23 11:57:02 +0100

    corrected cors error

--- RendezvousFrontend/src/App.jsx
+++ RendezvousFrontend/src/App.jsx
@@ -1,35 +1,37 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import RendezvousService from "./repository/RendezvousRepository";
 import "./styles/App.css";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  const loadUsers = () => {
+    RendezvousService.fetchUsers()
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const loadUsers = () => {
-        RendezvousService.fetchUsers().then((response) => {
-            setUsers(response.json().data);
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
-    useEffect(() => {
-        loadUsers();
-    }, [])
-
-    return <div className="app">
-        <h1>This is our app for now.</h1>
-        <br/>
-        <ul>
-            {users.map((user) =>
-                <li key={user.name}>
-                    {user.name} {user.surname}
-                </li>)}
-        </ul>
-
-    </div>;
+  return (
+    <div className="app">
+      <h1>This is our app for now.</h1>
+      <br />
+      <ul>
+        {users.map((user) => (
+          <li key={user.name}>
+            {user.name} {user.surname}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-
 
 export default App;
--- RendezvousFrontend/src/custom/axios.jsx
+++ RendezvousFrontend/src/custom/axios.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://localhost:9000/api",
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-    },
+  baseURL: "http://localhost:9000/korisniciApi",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default instance;
