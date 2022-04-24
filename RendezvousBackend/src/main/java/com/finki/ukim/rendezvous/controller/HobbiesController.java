188a0ffbf807bdb0788bf4457aa96b245c01f653
KralMarko123 <kralmarko123@gmail.com>
2022-04-25 00:22:23 +0200

    added editing enums, cross origin to other apis, some event logic

--- RendezvousBackend/src/main/java/com/finki/ukim/rendezvous/controller/HobbiesController.java
+++ RendezvousBackend/src/main/java/com/finki/ukim/rendezvous/controller/HobbiesController.java
@@ -5,15 +5,10 @@ import com.finki.ukim.rendezvous.service.HobbiesService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/hobbiesApi")
 public class HobbiesController {
     private final HobbiesService hobbiesService;
