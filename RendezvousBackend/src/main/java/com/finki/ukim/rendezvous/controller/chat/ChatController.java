e85e987a2fd2e1d7df127c8594a04177620c5c23
KralMarko123 <kralmarko123@gmail.com>
2022-04-27 17:30:00 +0200

    added cross origin

--- RendezvousBackend/src/main/java/com/finki/ukim/rendezvous/controller/chat/ChatController.java
+++ RendezvousBackend/src/main/java/com/finki/ukim/rendezvous/controller/chat/ChatController.java
@@ -6,8 +6,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class ChatController {
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
